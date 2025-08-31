90a7382d006267142a9cb0c45fcb7b7d42d94d90
00Fazee <121313122+Snxdfer@users.noreply.github.com>
2025-08-31 16:11:15 -0400

    Update example.lua

--- AirKeySys/example.lua
+++ AirKeySys/example.lua
@@ -1,4 +1,3 @@
 local AirKey = loadstring(game:HttpGet("https://raw.githubusercontent.com/Snxdfer/scripts/refs/heads/main/AirKeySys/source.lua"))()
-task.wait(1.5)
+task.wait(1)
 AirKey("Swoowsh", "An innovative script", "https://work.ink/4iC/ll8lemwj")
-print("Whitelisted!")
