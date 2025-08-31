427ebfa50bfa2c1a4d650dbb7d91c062215eb17a
00Fazee <121313122+Snxdfer@users.noreply.github.com>
2025-08-31 15:57:36 -0400

    Create example.lua

---
+++ AirKeySys/example.lua
@@ -0,0 +1,4 @@
+local AirKey = loadstring(game:HttpGet("https://raw.githubusercontent.com/Snxdfer/scripts/refs/heads/main/AirKeySys/source.lua")()
+task.wait(1.5)
+AirKey("Swoowsh", "An innovative script", "https://work.ink/4iC/ll8lemwj")
+print("Whitelisted!")
